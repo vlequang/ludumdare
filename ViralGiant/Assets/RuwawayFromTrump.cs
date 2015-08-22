c57879f7acf49a0c1189a358ec2a802700da49ae
Vincent Le Quang <vincent@storm8.com>
2015-08-22 13:08:03 -0700

    Dudes

---
+++ ViralGiant/Assets/RuwawayFromTrump.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RuwawayFromTrump : MonoBehaviour {
+
+	public Transform trump;
+	public float sightDistance;
+	public float repelDistance;
+	Rigidbody2D rigidBody;
+
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody2D> ();
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float distance = Vector3.Distance (trump.position, this.transform.position);
+		if (distance < sightDistance) {
+			Vector3 repel = this.transform.position - trump.position;
+			repel.Normalize ();
+			rigidBody.AddRelativeForce(repel * Time.deltaTime * repelDistance);
+			Debug.Log ("I see trump!");
+		} else {
+
+		}
+	}
+}
