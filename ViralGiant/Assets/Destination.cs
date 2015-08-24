d0276e45d8e3bd0244d3b98d83f9a626ed2d7274
sargrass <sargrass@gmail.com>
2015-08-23 23:47:18 -0700

    explode

--- ViralGiant/Assets/Destination.cs
+++ ViralGiant/Assets/Destination.cs
@@ -6,6 +6,7 @@ public class Destination : MonoBehaviour {
 	public GameObject Heart;
 	public GameObject Movie;
 	public float videoDelay = 3f;
+	public float explodeDelay = 2f;
 	public GameObject endingCells;
 
 	void Start () {
@@ -20,15 +21,20 @@ public class Destination : MonoBehaviour {
 		}
 	}
 
+	IEnumerator ExplodeCells() {
+		yield return new WaitForSeconds(explodeDelay);
+		if (endingCells) {
+			foreach (Transform child in endingCells.transform) {
+				child.GetComponent<Explosion>().Explode();
+				Destroy (child.gameObject);
+			}
+		}
+	}
+
 	void OnTriggerEnter2D(Collider2D other) {
 		if (other.tag=="Virus" && Heart != null) {
 			Heart.GetComponent<Animator>().SetTrigger("Win");
-			if (endingCells) {
-				foreach (Transform child in endingCells.transform) {
-					child.GetComponent<Explosion>().Explode();
-					Destroy (child.gameObject);
-				}
-			}
+			StartCoroutine ("ExplodeCells");
 			StartCoroutine ("PlayMovie");
 		}
 	}
